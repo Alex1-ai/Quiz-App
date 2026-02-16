cd32009dcf61c4cc7dbd9d77704413d4a012fb5b
Alex1-ai <alexanderemmanuel1719@gmail.com>
2026-02-16 12:03:48 +0100

    made some changes to script.groovy

--- script.groovy
+++ script.groovy
@@ -91,7 +91,8 @@ def githubCommit() {
             git commit -m "ci: version bump" || echo "No changes to commit"
 
             # Push directly to main (force if needed)
-            git push https://${GIT_USER}:${GIT_PASS}@github.com/Alex1-ai/Quiz-App.git HEAD:main
+            git pull
+            git push https://${GIT_USER}:${GIT_PASS}@github.com/Alex1-ai/Quiz-App.git HEAD:jenkins-job
         '''
     }
 }
